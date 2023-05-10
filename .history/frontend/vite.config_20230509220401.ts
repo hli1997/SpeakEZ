068d8770b9de12dfe5c807bbb6a81c1bc0b680a0
hli1997 <lihaodong97@gmail.com>
2023-05-09 22:07:42 -0400

    Change port

--- .history/frontend/vite.config_20230509220401.ts
+++ .history/frontend/vite.config_20230509220401.ts
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 8000
-  },
-  preview: {
-    port: 3030
-  },
   plugins: [react()],
 })
